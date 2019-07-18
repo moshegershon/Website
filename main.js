0dc813f6821435027245d86f42b893b47820427a
moshegershon <gershon46@gmail.com>
2019-07-18 16:28:05 +0300

    try 1 sticky nav bar

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
-<script src='node_modules/jquery/dist/jquery.min.js'></script>
- $(document).ready(function(){
-    $('#MyName').fadeIn("slo");
- });
+  <script src='node_modules/jquery/dist/jquery.min.js'></script>
+        $(document).ready(function () {
+            $("#CompanyName").fadeIn(2000);
+            $("#text").fadeIn(4000);
+            $('#HomeContent').fadeIn(4100);
+
+
+            $("#FirstEmployee").hover(function () {
+                $("#PicEmployee1").fadeOut("slow");
+                $("#dani").show();
+            },
+                function () {
+                    $("#PicEmployee1").fadeIn("slow");
+                    $("#dani").hide();
+                });
+
+
+            $("#SecondEmployee").hover(function () {
+                $("#PicEmployee2").fadeOut("slow");
+                $("#moshe").show();
+
+            },
+                function () {
+                    $("#PicEmployee2").fadeIn("slow");
+                    $("#moshe").hide();
+                });
+
+
+            $("#ThirdEmployee").hover(function () {
+                $("#PicEmployee3").fadeOut("slow");
+                $("#hanoh").show();
+            },
+                function () {
+                    $("#PicEmployee3").fadeIn("slow");
+                    $("#hanoh").hide();
+                });
+        });
